d828d193d130b70500f82c257adf1c3a896ee83f
Jack Turcotte <jack.turcotte@stu.ocsb.ca>
2025-02-14 18:09:31 +0000

    completed program, area_per_rect.cpp

---
+++ area_per_rect.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2025 Jack Turcotte All rights reserved.
+//
+// Created by: Jack Turcotte
+// Date: February 14, 2025
+// This program performs basic math operations
+
+#include <cmath>  // import the BEST math library (math.h is not updated as much as cmath)
+#include <iostream>
+
+int main() {
+    std::cout << "For a rectangle with an length of 2 meters, and a height of 5 meters: \n"
+              << std::endl;
+    std::cout << "The area is " << 2 * 5 << "m^2" << std::endl;
+    std::cout << "The perimeter is " << 2 * (2) + 2 * (5) << "m" << std::endl;
+}
